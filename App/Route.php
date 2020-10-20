58f8eb6f970aacc41b942e68e08c770f1dd6b83a
yvpf <yagovictorpessoafernandes2@gmail.com>
2020-10-20 17:12:59 -0300

    add files

---
+++ App/Route.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap {
+
+	protected function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$this->setRoutes($routes);
+	}
+
+}
+
+?>
